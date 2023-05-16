b97bc070de4b825d5f0d3b0b7ecfcada69695ec0
Gratix0 <oganisyan.sanya1@mail.ru>
2023-05-16 22:02:29 +0300

    1

---
+++ giff/app/src/main/java/com/example/giff/TweenAnim.java
@@ -0,0 +1,14 @@
+package com.example.giff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TweenAnim extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tween_anim);
+    }
+}
